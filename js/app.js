9e4b657cbc4510630115a9c7d28ea89f4a3e8edf
shadowANTRAX <55231753+shadowANTRAX@users.noreply.github.com>
2019-09-12 01:21:35 -0700

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+
+let navbar = document.querySelector(".navbar");
+let sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
